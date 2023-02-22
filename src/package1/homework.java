b896c67e4a64d1d5cc47a030746cad2c2dfb52cc
samoosama1 <samet_2003_tekin@hotmail.com>
2023-02-22 11:51:16 +0300

    first commit

--- src/package1/homework.java
+++ src/package1/homework.java
@@ -2,8 +2,6 @@ package package1;
 
 public class homework {
     public static void main(String[] args) {
-        myjavafile a = new myjavafile();
-        System.out.println("x is " + a.x);
-        
+                
     }
 }

c05c82208709476a09d6ec967132a853f2bbc8e3
samoosama1 <samet_2003_tekin@hotmail.com>
2023-02-22 18:51:54 +0300

    second commit

--- src/package1/homework.java
+++ src/package1/homework.java
@@ -2,6 +2,13 @@ package package1;
 
 public class homework {
     public static void main(String[] args) {
-                
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x is " + x);
+        }
+        else {
+            System.out.println("x is not " + x);
+        }
+        System.out.println("This part runs no matter what");
     }
 }
